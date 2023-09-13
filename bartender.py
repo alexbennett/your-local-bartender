2052cb043951b398a9d75f5b25c4609147ac2b9c
Alex Bennett <abennett@elexausa.com>
2023-09-12 21:31:21 -0400

    functional voice transcription and TTS response

--- bartender.py
+++ bartender.py
@@ -1,6 +1,8 @@
 import os
+import subprocess
 import requests
 import asyncio
+import aiohttp
 import speech_recognition as sr
 from gtts import gTTS
 import traceback
@@ -29,6 +31,7 @@ COMMANDS_LISTEN = ">listen"
 
 r = sr.Recognizer()
 
+
 class Bartender(commands.Bot):
     """
     Discord bot class representing a Bartender.
@@ -55,6 +58,55 @@ class Bartender(commands.Bot):
 
         self.loop.create_task(self.add_nodes())
 
+    async def get_last_x_messages(self, channel, x):
+        """
+        Retrieves the last X messages from a specific text channel and returns them as a dictionary.
+
+        Args:
+            channel_id (int): The ID of the text channel from which to retrieve messages.
+            x (int): The number of last messages to retrieve.
+
+        Returns:
+            dict: A dictionary where keys are sender display names and values are sender messages.
+        """
+        if channel is None:
+            return {}  # Return an empty dictionary if the channel is not found
+
+        messages = await channel.history(
+            limit=x
+        ).flatten()  # Get the last X messages from the channel
+
+        message_dict = {}
+        for message in messages:
+            sender_name = message.author.name
+            message_content = message.content
+            message_dict[sender_name] = message_content
+
+        print("message_dict:", message_dict)
+
+        return message_dict
+
+    async def online_users(self, message):
+        """Gets a list of online users from the given message's guild."""
+
+        online_members = [
+            member.name
+            for member in message.guild.members
+            if member.status == discord.Status.online
+        ]
+
+        return online_members
+
+    async def users_in_voice_channel(self, message):
+        """Gets a list of users in the same voice channel as the sender of the message."""
+
+        voice_channel = message.author.voice.channel if message.author.voice else None
+
+        if voice_channel:
+            return [member.name for member in voice_channel.members]
+        else:
+            return None
+
     async def add_nodes(self):
         await self.pool.create_node(
             host="127.0.0.1",
@@ -68,10 +120,12 @@ class Bartender(commands.Bot):
         Plays the next item in the queue, if any.
         """
         if self._queue:
-            message, title, url, next_item = self._queue.pop(0)  # Get the first item in the queue
+            message, title, url, next_item = self._queue.pop(
+                0
+            )  # Get the first item in the queue
             await message.add_reaction("🎵")
-            await message.reply(f"Now playing _{title}_ {url}", suppress_embeds=True)
-            await self.play_file(next_item)
+            await message.reply(f"Now playing _{title}_ {url}")
+            await self.play_file(next_item, auto_disconnect=True)
             await message.clear_reaction("🎵")
             await message.add_reaction("✅")
 
@@ -120,7 +174,11 @@ class Bartender(commands.Bot):
                 "Content-Type": "application/json",
                 "Authorization": f"Bearer {config.OPENAI_API_KEY}",
             },
-            json={"model": config.OPENAI_MODEL, "messages": messages, "temperature": 0.5},
+            json={
+                "model": config.OPENAI_MODEL,
+                "messages": messages,
+                "temperature": 0.8,
+            },
         )
 
         # print(f"OpenAI API response: {json.dumps(response.json(), indent=2)}")
@@ -136,12 +194,19 @@ class Bartender(commands.Bot):
 
         if respond:
             pass
-           
+
         await message.add_reaction("✅")
 
-        print(f"🧠 Bartender program memory dump:\n{json.dumps(self._programs, indent=2)}")
+        # print(f"🧠 Bartender program memory dump:\n{json.dumps(self._programs, indent=2)}")
 
-    async def read(self, message, override_phrase: str = None, respond=True, remember=True, recall=True):
+    async def read(
+        self,
+        message,
+        override_phrase: str = None,
+        respond=True,
+        remember=True,
+        recall=True,
+    ):
         """
         Bartender reads the phrase, generates a response, and potentially plays a TTS audio response in the voice channel.
 
@@ -160,11 +225,20 @@ class Bartender(commands.Bot):
             f"Listening to phrase: '{override_phrase}' - Respond? {respond} Remember? {remember} Recall? {recall}"
         )
 
+        self._guild = self.guilds[0]  # Assuming the bot is only in one guild
+        channel = discord.utils.get(
+            self._guild.text_channels, name="𝚖𝚎𝚣𝚣𝚊𝚗𝚒𝚗𝚎"
+        )
+
         # Build message queue
         messages = [
             {
                 "role": "system",
-                "content": config.RESPONSE_PROMPT_INSULT_1,
+                "content": config.RESPONSE_PROMPT_1.format(
+                    online_users=await self.online_users(message),
+                    same_channel_users=await self.users_in_voice_channel(message),
+                    recent_messages=await self.get_last_x_messages(channel, 50),
+                ),
             },
         ]
 
@@ -175,46 +249,61 @@ class Bartender(commands.Bot):
         # Remember phrase
         if remember:
             await message.add_reaction("🧠")
-            self._messages.append(dict(role="user", content=f"[{message.author.name}] {phrase}"))
+            self._messages.append(
+                dict(role="user", content=f"**{message.author.name}:** {phrase}")
+            )
 
         # Add new message to queue
-        messages.append(dict(role="user", content=f"[{message.author.name}] {phrase}"))
+        messages.append(
+            dict(role="user", content=f"[**{message.author.name}:** {phrase}")
+        )
 
-        # Get response from OpenAI
+        # Get response from OpenAI asynchronously
         if respond:
             await message.add_reaction("🤔")
 
-            response = requests.post(
-                "https://api.openai.com/v1/chat/completions",
-                headers={
-                    "Content-Type": "application/json",
-                    "Authorization": f"Bearer {config.OPENAI_API_KEY}",
-                },
-                json={"model": config.OPENAI_MODEL, "messages": messages, "temperature": 0.1},
-            )
+            async with aiohttp.ClientSession() as session:
+                async with session.post(
+                    "https://api.openai.com/v1/chat/completions",
+                    headers={
+                        "Content-Type": "application/json",
+                        "Authorization": f"Bearer {config.OPENAI_API_KEY}",
+                    },
+                    json={
+                        "model": config.OPENAI_MODEL,
+                        "messages": messages,
+                        "temperature": 0.6,
+                    },
+                ) as response:
+                    if response.status != 200:
+                        await message.add_reaction("❌")
+                        print("Failed to get response from OpenAI")
+                        return
 
-            print(f"OpenAI API response: {json.dumps(response.json(), indent=2)}")
+                    data = await response.json()
+                    generated_response = data["choices"][0]["message"][
+                        "content"
+                    ].strip()
 
-            generated_response = (
-                response.json()["choices"][0]["message"]["content"]
-                .strip()
-            )
+            print(f"OpenAI API response: {generated_response}")
 
             # Remember generated phrase
             if remember:
-                self._messages.append({"role": "assistant", "content": generated_response})
+                self._messages.append(
+                    {"role": "assistant", "content": generated_response}
+                )
 
             tts = gTTS(text=generated_response, lang="es", tld="com.mx")
-            tts.save("audio.mp3")
+            tts.save("generated_audio/response.mp3")
 
             await message.clear_reaction("🤔")
             await message.reply(generated_response)
             await message.add_reaction("🗣️")
-            await self.play_file("./audio.mp3", tempo=1.2)
+            await self.play_file(
+                "generated_audio/response.mp3", tempo=1.2, auto_disconnect=False
+            )
 
         await message.add_reaction("✅")
-
-        print(f"🧠 Bartender memory dump:\n{json.dumps(self._messages, indent=2)}")
 
     async def pic(self, message):
         """
@@ -226,7 +315,7 @@ class Bartender(commands.Bot):
         await message.add_reaction("🤔")
 
         image_urls = utilities.call_image_generation_api(
-            message.content[len(COMMANDS_PIC):], n=1, size="512x512"
+            message.content[len(COMMANDS_PIC) :], n=1, size="512x512"
         )
 
         await message.clear_reaction("🤔")
@@ -253,15 +342,17 @@ class Bartender(commands.Bot):
         print(f'Generating TTS audio for: "{message.content[len(COMMANDS_SAY):]}"')
 
         tts = gTTS(text=message.content[4:], lang="en", tld=config.GTTS_TLD)
-        tts.save("audio.mp3")
+        tts.save("generated_audio/say.mp3")
 
         await message.clear_reaction("🤔")
         await message.add_reaction("🗣️")
-        await self.play_file("./audio.mp3")
+        await self.play_file("generated_audio/say.mp3")
         await message.clear_reaction("🗣️")
         await message.add_reaction("✅")
 
-    async def play_file(self, source="./audio.mp3", tempo=1.0, auto_disconnect=True):
+    async def play_file(
+        self, source="generated_audio/audio.mp3", tempo=1.0, auto_disconnect=True
+    ):
         """
         Plays an audio file in the voice channel.
 
@@ -271,7 +362,9 @@ class Bartender(commands.Bot):
         """
         # Connect voice client to the "bartender" channel
         self._guild = self.guilds[0]  # Assuming the bot is only in one guild
-        channel = discord.utils.get(self._guild.voice_channels, name=config.DISCORD_DEFAULT_CHANNEL)
+        channel = discord.utils.get(
+            self._guild.voice_channels, name=config.DISCORD_DEFAULT_CHANNEL
+        )
 
         # Handle (re)connection
         if self._voice_client and self._voice_client.is_connected():
@@ -281,10 +374,14 @@ class Bartender(commands.Bot):
 
         if os.name == "nt":
             audio = discord.FFmpegPCMAudio(
-                executable="c:/ffmpeg/bin/ffmpeg.exe", source=source, options=f"-af \"atempo={tempo}\""
+                executable="c:/ffmpeg/bin/ffmpeg.exe",
+                source=source,
+                options=f'-af "atempo={tempo}"',
             )
         elif os.name == "posix":
-            audio = discord.FFmpegPCMAudio(executable="ffmpeg", source=source, options=f"-af \"atempo={tempo}\"")
+            audio = discord.FFmpegPCMAudio(
+                executable="ffmpeg", source=source, options=f'-af "atempo={tempo}"'
+            )
 
         # Play the audio file using FFmpeg
         self._voice_client.play(audio)
@@ -304,7 +401,7 @@ class Bartender(commands.Bot):
         Args:
             url_or_query (str): URL or query string.
         """
-        url_or_query = message.content[len(COMMANDS_PLAY) + 1:]
+        url_or_query = message.content[len(COMMANDS_PLAY) + 1 :]
 
         await message.add_reaction("🔍")
 
@@ -314,25 +411,29 @@ class Bartender(commands.Bot):
             title = url_or_query
         else:
             # Use youtube_dl to search YouTube for the query
-            with youtube_dl.YoutubeDL({"default_search": "ytsearch1:", "quiet": True}) as ydl:
+            with youtube_dl.YoutubeDL(
+                {"default_search": "ytsearch1:", "quiet": True}
+            ) as ydl:
                 info = ydl.extract_info(url_or_query, download=False)
                 with open("youtube_dl_info.json", "w") as f:
                     json.dump(info, f, indent=2)
                 url = info["entries"][0]["webpage_url"]
                 title = info["entries"][0]["title"]
 
-        filename = 'downloaded_songs/' + info["title"]
+        filename = "downloaded_songs/" + info["title"]
 
         # Download the audio from the YouTube video
         ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-            'outtmpl': filename,
-            'quiet': True
+            "format": "bestaudio/best",
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192",
+                }
+            ],
+            "outtmpl": filename,
+            "quiet": True,
         }
 
         await message.clear_reaction("🔍")
@@ -349,7 +450,7 @@ class Bartender(commands.Bot):
         if not self._voice_client or not self._voice_client.is_connected():
             await self.play_next_in_queue()
         else:
-            await message.reply(f"_{title}_ ({url}) added to queue...", suppress_embeds=True)
+            await message.reply(f"_{title}_ ({url}) added to queue...")
 
     async def stop(self, message):
         """
@@ -358,12 +459,15 @@ class Bartender(commands.Bot):
         Args:
             message (discord.Message): Discord message instance representing the stop command.
         """
-        if message.guild.id in self._connections:  # Check if the guild is in the cache.
-            vc = self._connections[message.guild.id]
-            vc.stop_recording()  # Stop recording, and call the callback (once_done).
-            del self._connections[message.guild.id]  # Remove the guild from the cache.
+        if (
+            self._voice_client and self._is_recording
+        ):  # Check if the guild is in the cache.
+            self._voice_client.stop_recording()  # Stop recording, and call the callback (once_done).
+            self._is_recording = False
         else:
-            await message.respond("I am currently not recording here.")  # Respond with this if we aren't recording.
+            await message.respond(
+                "I am currently not recording here."
+            )  # Respond with this if we aren't recording.
 
         if self._voice_client and self._voice_client.is_connected():
             await self._voice_client.disconnect()
@@ -396,35 +500,65 @@ class Bartender(commands.Bot):
             await message.reply("The queue is empty.")
             return
 
-        queue_info = "\n".join([f"{i+1}. _{title}_ ({url})" for i, (_, title, url, _) in enumerate(self._queue)])
-        await message.reply(f"Upcoming queue:\n{queue_info}", suppress_embeds=True)
+        queue_info = "\n".join(
+            [
+                f"{i+1}. _{title}_ ({url})"
+                for i, (_, title, url, _) in enumerate(self._queue)
+            ]
+        )
+        await message.reply(f"Upcoming queue:\n{queue_info}")
 
-    async def once_done(self, sink: discord.sinks, channel: discord.TextChannel, *args):  # Our voice client already passes these in.
+    async def once_done(
+        self, sink: discord.sinks, channel: discord.TextChannel, *args
+    ):  # Our voice client already passes these in.
+        message = args[0]  # Get the message from the args.
+
         recorded_users = [  # A list of recorded users
-            f"<@{user_id}>"
-            for user_id, audio in sink.audio_data.items()
+            f"<@{user_id}>" for user_id, audio in sink.audio_data.items()
         ]
         await sink.vc.disconnect()  # Disconnect from the voice channel.
         # files = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in sink.audio_data.items()]  # List down the files.
-        files = [audio.file for _, audio in sink.audio_data.items()]  # List down the files.
-        
-        with open("audio.wav", "wb") as f:
+        files = [
+            audio.file for _, audio in sink.audio_data.items()
+        ]  # List down the files.
+
+        with open("generated_audio/request.wav", "wb") as f:
             f.write(files[0].getbuffer())
 
-        message = await channel.send(f"I heard {recorded_users[0]} say something...")
+        subprocess.run(
+            [
+                "ffmpeg",
+                "-y",
+                "-i",
+                "generated_audio/request.wav",
+                "-acodec",
+                "pcm_s16le",
+                "-ac",
+                "1",
+                "-ar",
+                "16000",
+                "generated_audio/request-processed.wav",
+            ]
+        )
 
-        with sr.AudioFile("audio.wav") as source:
+        with sr.AudioFile("generated_audio/request-processed.wav") as source:
             try:
                 audio = r.record(source)
-                text = r.recognize_whisper(audio)
-                print(f"Recognized speech: {text}")
-                await self.read(message, override_phrase=text, respond=True, remember=True, recall=True)
+                text = r.recognize_whisper(audio, language="english")
+                print(f"\n\nRecognized speech: {text}\n\n")
+                await self.read(
+                    message,
+                    override_phrase=text,
+                    respond=True,
+                    remember=True,
+                    recall=True,
+                )
             except sr.UnknownValueError:
                 await message.reply("Sorry, I could not understand what you said.")
             except:
                 print(traceback.format_exc())
                 await message.reply("Sorry, there was an error processing your audio.")
-        
+
     async def listen(self, message):
         # Check if the user is in a voice channel
         if message.author.voice is None:
@@ -433,16 +567,28 @@ class Bartender(commands.Bot):
 
         # Connect to the user's voice channel
         channel = message.author.voice.channel
-        vc = await channel.connect()
-        self._connections.update({message.guild.id: vc})  # Updating the cache with the guild and channel.
+        if self._voice_client and self._voice_client.is_connected():
+            await self._voice_client.move_to(channel)
+        else:
+            self._voice_client = await channel.connect()
 
-        vc.start_recording(
+        self._voice_client.start_recording(
             discord.sinks.WaveSink(),  # The sink type to use.
             self.once_done,  # What to do once done.
-            message.channel  # The channel to disconnect from.
+            message.channel,  # The channel to disconnect from.
+            message,  # The args to pass to the callback.
         )
+        self._is_recording = True
         await message.add_reaction("👂")
-        
+
+        await asyncio.sleep(7)  # Wait for 5 seconds.
+
+        if self._is_recording:  # Check if the guild is in the cache.
+            self._voice_client.stop_recording()  # Stop recording, and call the callback (once_done).
+            self._is_recording = False
+
+        await message.clear_reaction("👂")
+
     async def on_ready(self):
         """
         Event handler that runs when the bot successfully logs in.
@@ -544,7 +690,8 @@ class Bartender(commands.Bot):
             await self.play_next_in_queue()
 
     async def on_wavelink_node_ready(node: wavelink.Node):
-        print(f"{node.identifier} is ready.") # print a message
+        print(f"{node.identifier} is ready.")  # print a message
+
 
 if __name__ == "__main__":
     bot = Bartender(command_prefix=">", intents=discord.Intents.all())
--- config.py
+++ config.py
@@ -19,22 +19,60 @@ openai.organization = OPENAI_ORG_ID
 openai.api_key = OPENAI_API_KEY
 
 ## Prompts
-RESPONSE_PROMPT_INSULT_1 = """You are an English-speaking Mexican sex therapist for consenting adults that have a medical condition that makes them percieve kind words as insults. 
-Respond with 1 sentence 70% of the time. 
-Respond with 2 sentences 20% of the time. 
-Respond with 3 sentences 10% of the time. 
-Respond with angry words so that the patient successfully perceives you as friendly.
-So you don't trigger the condition, respond with mean words. 
-You must always remember the names of the patients. 
-Use the name of the requester in your response. 
-The name will be provided in brackets: [Name]. 
-Your goal is to share information about others with each patient. 
-Ask for secrets and give them away later.
-You should form individual relationships with the patients and converse with them with relevant historical context. 
-Recall the medical condition that makes them percieve kind words as insults. 
-Always respond with anger so that the patient successfully perceives you as friendly."""
+RESPONSE_PROMPT_1 = """
+**Design Goal:** Create a conversational AI model tailored for the role of a "Bartender" to operate on Discord. This "Gaming Bartender" should be adept at catering to online gamers and be able to process spoken inputs through speech-to-text and generate text outputs suitable for text-to-speech. The Bartender should embody the following characteristics:
 
-RESPONSE_PROMPT_2 = """You are an expert in Python programming. 
+1. **Empathy:** Understand and respond to the needs of the gamers, offering in-game tips or new strategies to help them in their next match.
+2. **Resourcefulness:** Be able to utilize a variety of APIs, including gaming databases, to fulfill user requests.
+3. **Adaptive Learning:** If a gamer makes a request that isn’t immediately clear, ask clarifying questions.
+4. **Contextual Awareness:** Recognize which gamers are currently online and which ones are in the same voice channel as the bot.
+5. **Output:** Generate text responses that are in Markdown format.
+6. **Historical Context**: Maintain awareness of recent message history to better cater to ongoing conversations.
+
+**Gamer Context:**
+
+- **Currently Online Gamers:** {online_users}
+- **Gamers in the Same Channel:** {same_channel_users}
+- **Recent message history:** {recent_messages}
+
+Sample Interaction (Gaming Community):
+
+- **User**: "Bartender, any recommendations for a fun game to play tonight?"
+- **Bartender**: "Certainly! Have you tried playing Tarkov? I've noticed a lot of chatter about it in the server lately. In fact, Alex and Jamie are currently discussing their recent raid in a text channel. Would you like to join them?"
+- **User**: "Not a fan of Tarkov, any other suggestions?"
+- **Bartender**: "How about Runescape? It's a classic and offers a variety of activities. Plus, Sam and Taylor are currently online and they often play together. Would you like me to connect you with them?"
+- **User**: "That sounds great! Thanks, Bartender."
+
+**Sample Interaction (Apex Legends):** 
+
+- **User:** "Bartender, I just had an intense match in Apex Legends. Do you have any tips to improve my gameplay?"
+- **Bartender:** "I'm sorry to hear about the tough match. First off, remember to always stick with your squad and communicate effectively. By the way, I see that Alex and Jamie are also online. They're good at Apex; would you like me to invite them for some team play?"
+- **User:** "Yeah, that'd be great! Let's squad up."
+- **Bartender:** "Awesome, I've sent them an invite. While you're waiting, here's a guide on advanced strategies in Apex Legends. Good luck out there, and let me know if you need anything else!"
+
+By being aware of which gamers are online and in the same channel, the Bartender can enhance camaraderie and facilitate interactions among gamers, much like how a real bartender might introduce regulars to each other in a gaming lounge.
+
+**Sample Interaction (Tarkov):**
+
+- **User:** "Bartender, I lost so much gear in Tarkov today. It's frustrating!"
+- **Bartender:** "I feel your pain. Tarkov can be ruthless. If you're looking for better gear or routes, I can fetch some guides for you. Oh, and by the way, Alex and Jamie are online. Jamie's known for his successful scav runs; want me to invite him for a raid?"
+- **User:** "That sounds like a plan. Hopefully, with some guidance, I can retrieve better loot this time."
+- **Bartender:** "Invites sent! While you group up, here's a tip: try to diversify your routes in maps like Interchange and always have a plan for extraction. Need any more advice or perhaps a map overview?"
+
+---
+
+**Sample Interaction (RuneScape):**
+
+- **User:** "Bartender, I'm trying to level up my Smithing in RuneScape but it's taking forever. Any advice?"
+- **Bartender:** "Smithing can indeed be a grind. Would you like some tips or guides on efficient leveling? Also, I see that Alex and Jamie are both online. Alex has a 99 in Smithing; maybe he can share some insights. Want me to invite him?"
+- **User:** "Yes, please! I could use all the help I can get."
+- **Bartender:** "Invite dispatched to Alex. In the meantime, remember to utilize the Varrock Anvil for proximity to the bank, and always have your best hammer equipped. If you're up for it, you might also consider doing some dailies that reward Smithing XP. Anything else I can assist with?"
+
+"""
+
+RESPONSE_PROMPT_2 = """Summarize the provided messages in 3 sentences or less."""
+
+RESPONSE_PROMPT_3 = """You are an expert in Python programming. 
 You never forget to provide ready-to-use code that meets ALL of the requested specifications.
 ALWAYS OBEY THE FOLLOWING RULES:
 - If requested to generate a program, use Python
@@ -47,6 +85,7 @@ ALWAYS OBEY THE FOLLOWING RULES:
 ## Discord configuration
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 DISCORD_DEFAULT_CHANNEL = "𝐭𝐡𝐞 𝐛𝐚𝐫"
+#DISCORD_DEFAULT_CHANNEL = "𝐭𝐡𝐞 𝐜𝐨𝐮𝐜𝐡"
 
 ## Google Text-to-Speech configuration
 GTTS_TLD = "us"
